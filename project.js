d55d1cc3b1923e79e3bde4835b80d589cd68d631
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-04-11 12:26:02 +0900

    UPDATE::ejs 데이터 전송

--- project.js
+++ project.js
@@ -5,15 +5,18 @@ const API_KEY = '57b0311f6cedbd7612a7a70c7bb27bab'
 
 const app = express()
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
 app.post('/getWeatherInfo', (req, res) => {
-  const {lon, lat} = req.body
+  const { lon, lat } = req.body
 
   const options = {
     uri: 'https://api.openweathermap.org/data/2.5/onecall',
@@ -23,14 +26,27 @@ app.post('/getWeatherInfo', (req, res) => {
       units: 'metric',
       lon: lon,
       lat: lat,
-    }
-  };
-  request(options, (err, response, body) => {
-    console.log(err)
-    console.log(response)
-    console.log(body)
+    },
+  }
 
-    res.send(response)
+  request(options, (err, response, body) => {
+    const data = JSON.parse(body)
+    console.log(data)
+    const timezone = data["timezone"]
+    const main = data['current']['weather'][0]['main']
+    const temp = data['current']['temp']
+    const pressure = data['current']['pressure']
+    const humidity = data['current']['humidity']
+
+    console.log(timezone)
+
+    res.render('index', {
+      timezone : timezone,
+      main: main,
+      temp: temp,
+      pressure: pressure,
+      humidity: humidity,
+    })
   })
 })
 
